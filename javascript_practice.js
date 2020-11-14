9a239339ea1ace77c0defda7f4d758dc2a0ca814
Yuman <yumannasim_a@srmuniv.edu.in>
2020-11-14 18:53:28 +0530

    modified the name

---
+++ javascript_practice.js
@@ -0,0 +1,11 @@
+console.log("hello world! , are you there!!");
+//document.write("this is document")
+var name ="yuman"
+console.log(name);
+var a;
+var b= 2;
+a="this is me "+4;
+c=a+b;
+
+console.log(c);
+
