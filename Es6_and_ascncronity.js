be384f6b73e64b87f8b9b0f9768d5f2127ce9937
yuman <yumannasim_a@srmuniv.edu.in>
2021-01-03 02:13:06 +0530

    feat(Es6):part-3

--- Es6_and_ascncronity.js
+++ Es6_and_ascncronity.js
@@ -1,115 +1,115 @@
-// y=[1,2,3,4];
+y=[1,2,3,4];
 
-// console.log(...y);
-// console.log(y);
+console.log(...y);
+console.log(y);
 
 
 
-// function createQuote(quote, callback){ 
-//       var myQuote = "Like I always say, " + quote;
-//       callback(myQuote); // 2
-//     };
+function createQuote(quote, callback){ 
+      var myQuote = "Like I always say, " + quote;
+      callback(myQuote); // 2
+    };
     
-//     function logQuote(quote){
-//       console.log(quote);
-//     };
+    function logQuote(quote){
+      console.log(quote);
+    };
     
-//     createQuote("eat your vegetables!", logQuote); 
+    createQuote("eat your vegetables!", logQuote); 
 
 
-//     var callback = function() {
-//             console.log("Hello World!");
-//         }
+    var callback = function() {
+            console.log("Hello World!");
+        }
         
-//         setTimeout(callback, 1000);
+        setTimeout(callback, 1000);
 
-//         var array1 = ['a', 'b', 'c'];
-//         array1.forEach(function(element) {
-//               console.log(element);
-//             });
+        var array1 = ['a', 'b', 'c'];
+        array1.forEach(function(element) {
+              console.log(element);
+            });
             
 
-//             var fs = require("fs");
+            var fs = require("fs");
 
-// fs.readFile("helper.txt", function(err, data) {
-//   if (err) return console.error(err);
-//   else console.log(data.toString());
-// });
+fs.readFile("helper.txt", function(err, data) {
+  if (err) return console.error(err);
+  else console.log(data.toString());
+});
 
 
 
-// var division=(num1,num2,callback)=>{
+var division=(num1,num2,callback)=>{
     
-//       if(num2!=0)
-//       {
-//          var res=num1/num2;
-//          return callback("no error",res); 
-//       }
-//     //   else{
-//     //     var  res=null;
-//     //       return callback("num2 shouldn't be zero",res);
-//     //   }
+      if(num2!=0)
+      {
+         var res=num1/num2;
+         return callback("no error",res); 
+      }
+    //   else{
+    //     var  res=null;
+    //       return callback("num2 shouldn't be zero",res);
+    //   }
   
-//   }
+  }
   
-//   var callback=(err,result)=>{
-//       if(result==null)
-//        return err;
+  var callback=(err,result)=>{
+      if(result==null)
+       return err;
   
-//        else{
-//            return result;
-//        }
+       else{
+           return result;
+       }
   
-//   }
+  }
 
-//   console.log(division(4,4,callback));
-
-
-
-// var add=(x,y)=>{
-//     return x+y;
-//     }
-// var sub=(x,y)=>{
-//  return x-y;
-// }
+  console.log(division(4,4,callback));
 
 
-// var calc=(x,y,callback)=>{
+
+var add=(x,y)=>{
+    return x+y;
+    }
+var sub=(x,y)=>{
+ return x-y;
+}
+
+
+var calc=(x,y,callback)=>{
   
-//   if(callback==add)
-//   return add(x,y);
+  if(callback==add)
+  return add(x,y);
 
-//   else{
-//       return sub(x,y);
-//   }
-// }
+  else{
+      return sub(x,y);
+  }
+}
 
-// console.log(calc(6,5,sub));
+console.log(calc(6,5,sub));
 
-// const posts = [{ title: 'Post one' , body:'This is post one' }];
+const posts = [{ title: 'Post one' , body:'This is post one' }];
 
-// console.log(posts);
+console.log(posts);
 
-// var createPost=(post)=>{
-//      posts.push(post);
-//      return posts;
+var createPost=(post)=>{
+     posts.push(post);
+     return posts;
 
-// }
-// setTimeout(createPost, 2000);
-
-
-// var getPost=()=>{
-//     return posts.pop();
-// }
-// setTimeout(getPost, 1000);
+}
+setTimeout(createPost, 2000);
 
 
-// console.log(createPost({ title: 'Post one' , body:'This is post one' }));
-// console.log(createPost({ title: 'Post one' , body:'This is post one' }));
+var getPost=()=>{
+    return posts.pop();
+}
+setTimeout(getPost, 1000);
 
-// console.log(createPost({ title: 'Post one' , body:'This is post one' }));
 
-// console.log(getPost({ title: 'Post one' , body:'This is post one' }));
+console.log(createPost({ title: 'Post one' , body:'This is post one' }));
+console.log(createPost({ title: 'Post one' , body:'This is post one' }));
+
+console.log(createPost({ title: 'Post one' , body:'This is post one' }));
+
+console.log(getPost({ title: 'Post one' , body:'This is post one' }));
 
 var arr123 = [1, 2, 3, 4, 5, 6, 5, 5, 5, 1, 2, 2, 10, 10, 12, 10, 10, 1, 1, 1, 10];
 
@@ -145,51 +145,51 @@ if(callback==filter10)
 
 }
 
-// console.log(filter10(arr123));
-// console.log(arr123);
+console.log(filter10(arr123));
+console.log(arr123);
 
-// Promise                  
-// A Promise structure has 2 parts
-//First part
-//create the Promise and define the success/
+Promise                  
+A Promise structure has 2 parts
+First part
+create the Promise and define the success/
 
-// let promise1 = new Promise( (resolve, reject) => {
+let promise1 = new Promise( (resolve, reject) => {
 
-//     let dataReceivedSuccessfully = false;
-//     if (!dataReceivedSuccessfully)
-//       resolve('Data Available!');
-//     if (dataReceivedSuccessfully)
-//       reject('Data Corrupted!');
-//     })
+    let dataReceivedSuccessfully = false;
+    if (!dataReceivedSuccessfully)
+      resolve('Data Available!');
+    if (dataReceivedSuccessfully)
+      reject('Data Corrupted!');
+    })
     
-//     //Second part
-//     //define the actions for when the conditions are fulfilled
+    //Second part
+    //define the actions for when the conditions are fulfilled
     
-//       promise1.then( (message) => {
-//        console.log(message);
-//        }).catch( (message) => {
-//           console.log(message);
-//     })
+      promise1.then( (message) => {
+       console.log(message);
+       }).catch( (message) => {
+          console.log(message);
+    })
 
 
-// let s="Hello";
+let s="Hello";
 
-// let hello=(x)=>{
+let hello=(x)=>{
    
-//    return new Promise((reslove,reject)=>{
-//           if(x=="Hello")
-//            reslove(x+" World!");
-//            else
-//             reject("Bye!");
-//    })
-// }
+   return new Promise((reslove,reject)=>{
+          if(x=="Hello")
+           reslove(x+" World!");
+           else
+            reject("Bye!");
+   })
+}
 
 
-// hello(s).then((res)=>{
-//        console.log(res);
-//  }) .catch((err)=>{
-//      console.log(err);
-//  })
+hello(s).then((res)=>{
+       console.log(res);
+ }) .catch((err)=>{
+     console.log(err);
+ })
 
 
 let erro=(x)=>{
@@ -239,3 +239,209 @@ readFile("helper.txt")
     console.log(err);
   });
 
+
+let loadData= async (value)=>{
+     
+    if(value>0)
+    {
+  var obj={
+      data:value
+  } 
+  return obj;
+    }
+    else{
+        throw new Error("Value must be greater than 0");
+    }
+
+}
+
+loadData(-34).then((e)=>{
+    console.log(e);
+}).catch((e)=>{
+    console.log(e.message)
+})
+let str1=(str)=>{
+
+    return new Promise((resolve)=>{
+             setTimeout(()=>{
+             resolve(str);
+             },1000) 
+            });
+ 
+ }
+ 
+ 
+ let f1= async (str)=>{
+    const x= await str1(str);    
+    return x;
+ }
+ 
+ f1("done!").then((e)=>{
+     console.log(e);
+ })
+let func1=(st)=>{
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+      resolve(st);
+    },5000)
+})
+  }
+  
+  let func2=(st)=>{
+    
+      return st; 
+  }
+  
+  let concatenated= async(str1,str2)=>{
+      var st1="",st="";
+    st=await func1(str1);
+    
+    st1=func2(str2);
+    
+    
+   
+    return st+st1;
+  }
+  
+
+  concatenated("hi","hello").then((s)=>{
+   console.log(s);
+  });
+function doubleAfter2Seconds(x) {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve(x * 2);
+        }, 2000);
+      });
+    }
+    
+    async function addAsync(x, y, z) {
+      const a = await doubleAfter2Seconds(x);
+      const b = await doubleAfter2Seconds(y);
+      const c = await doubleAfter2Seconds(z);
+      return a + b + c;
+    }
+    
+    addAsync(100, 200, 300).then(sum => {
+      console.log(sum);
+    });
+let arr1=[];
+
+var insert=(ch)=>{
+    if(arr1.includes(ch))
+    return;
+    else{
+  return new Promise((resolve,reject)=>{
+       setTimeout(()=>{
+            resolve(arr1.push(ch));
+       },1000);
+  })
+}
+}
+
+var adding= async (arr)=>{
+     for(let i=0;i<arr.length;i++)
+     {
+          await insert(arr[i]);
+     }
+   
+}
+
+adding( [
+      1, 1, 2, 2, 3, 3,4, 4, 5, 5, 6, 6, 7]).then(()=>arr1);
+
+
+function *naturalnum() {
+    let i=1;
+     while(true)
+{
+    yield i++;
+}
+}
+
+
+var obj = naturalnum();
+x=1;
+while (x<=50) {
+  console.log(obj.next());
+x++;
+}
+
+
+function *generatorFn(num){
+     
+    while(num>=0)
+    { yield num;
+     num--;}
+
+
+}
+
+var rever = generatorFn(45);
+i=45
+while(i>=0)
+{console.log(rever.next());
+i--;}
+
+function* fib(num) {
+    var num1=0; 
+    var num2=1; 
+    var sum; 
+ 
+    if(num==0)
+    {
+        yield num1;
+    }
+ if(num==1)
+    {
+        yield num2;
+    }
+    else{
+   
+    for (let i = 2; i < num; i++)  
+    { 
+        sum=num1+num2; 
+        num1=num2; 
+        num2=sum; 
+    } 
+    yield num2;
+    }
+}
+
+for(let i=0;i<25;i++)
+{
+    var on= fib(i);
+    console.log(on.next());
+// }
+
+function* filter(array) {
+      let arry = array;
+      let i = 0;
+      while (i < 3) {
+        arry[i] = 0;
+        yield arry;
+        i++;
+      }
+    }
+    
+    gen = filter([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+   
+    for (let num of gen) {
+      console.log(num);
+    }
+    
+
+function* filter(array) {
+      let arry = array;
+      let i = 0;
+
+   for(let i=0;i<arry.length;i++)
+     
+   
+    }
+    
+    var arr=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    gen = filter(arr);
+    
+  console.log(gen.next());
+ 
